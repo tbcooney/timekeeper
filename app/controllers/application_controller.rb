6d0019a198fafd295a166fb781eff6906141e898
tbcooney <taylor.cooney@vegastudios.ca>
2015-04-30 23:19:29 -0400

    Set mailer host and devise invitable add to membership

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate_user!, :set_mailer_host
+
+private
+  def set_mailer_host
+    email = current_user ? "#{current_user.email}." : ""
+    ActionMailer::Base.default_url_options[:host] = "#{email}lvh.me:3000"
+  end
+
+  def after_invite_path_for(resource)
+    root_url
+  end
 end
--- db/migrate/20150501024657_devise_invitable_add_to_memberships.rb
+++ db/migrate/20150501024657_devise_invitable_add_to_memberships.rb
@@ -1,6 +1,6 @@
-class DeviseInvitableAddToUsers < ActiveRecord::Migration
+class DeviseInvitableAddToMemberships < ActiveRecord::Migration
   def up
-    change_table :users do |t|
+    change_table :memberships do |t|
       t.string     :invitation_token
       t.datetime   :invitation_created_at
       t.datetime   :invitation_sent_at
@@ -14,14 +14,14 @@ class DeviseInvitableAddToUsers < ActiveRecord::Migration
     end
 
     # And allow null encrypted_password and password_salt:
-    change_column_null :users, :encrypted_password, true
+    change_column_null :memberships, :encrypted_password, true
   end
 
   def down
-    change_table :users do |t|
+    change_table :memberships do |t|
       t.remove_references :invited_by, :polymorphic => true
       t.remove :invitations_count, :invitation_limit, :invitation_sent_at, :invitation_accepted_at, :invitation_token, :invitation_created_at
     end
-    change_column_null    :users, :encrypted_password, false
+    change_column_null    :memberships, :encrypted_password, false
   end
 end
